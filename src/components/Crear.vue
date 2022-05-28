e3d206d53b74c9f289d85ec7294bfa381aa8120e
Mauricio Batalla <maurybatalla@gmail.com>
2022-05-28 12:50:24 -0300

    primer commit

---
+++ src/components/Crear.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Nuevo Empleados
+            </div>
+            <div class="card-body">
+                <form v-on:submit.prevent="agregarRegistro">
+                    <div class="form-group">
+                      <label for="">Nombre: </label>
+                      <input type="text"
+                        class="form-control" name="nombre" required v-model="empleado.nombre" id="nombre" aria-describedby="helpId" placeholder="">
+                      <small id="helpId" class="form-text text-muted">Ingrese el nombre del empleado</small>
+                    </div>
+                     <div class="form-group">
+                      <label for="">Correo: </label>
+                      <input type="email"
+                        class="form-control" name="correo" required v-model="empleado.correo" id="correo" aria-describedby="helpId" placeholder="">
+                      <small id="helpId" class="form-text text-muted">Ingrese Email</small>
+                    </div>
+
+                    <div class="btn-group" role="group" aria-label="">
+                        <button type="submit" class="btn btn-success">Agregar </button>
+                        <router-link :to="{name:'Listar'}" class="btn btn-warning">Cancelar</router-link>
+                       
+                    </div>
+
+                </form>
+            </div>
+            <div class="card-footer text-muted">
+                Estudio Software
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            empleado:{}
+        }
+    },
+    methods:{
+        agregarRegistro(){
+            console.log(this.empleado);
+
+            var datosEnviar={nombre:this.empleado.nombre,correo:this.empleado.correo}
+
+             fetch('http://www.svr1.ar/empleados/?insertar=1',{
+                 method:"POST",
+                 body:JSON.stringify(datosEnviar)
+             })
+            .then(respuesta=>respuesta.json())
+            .then((datosRespuesta=>{
+                console.log(datosRespuesta);
+                window.location.href='listar'
+            }))
+        }
+        
+    }
+}
+</script>
---
+++ src/components/Editar.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Editar Empleados
+            </div>
+            <div class="card-body">
+                <form v-on:submit.prevent="actualizarRegistro">
+                    <div class="form-group">
+                      <label for="">Nombre: </label>
+                      <input type="text"
+                        class="form-control" name="nombre" required v-model="empleado.nombre" id="nombre" aria-describedby="helpId" placeholder="">
+                      <small id="helpId" class="form-text text-muted">Ingrese el nombre del empleado</small>
+                    </div>
+                     <div class="form-group">
+                      <label for="">Correo: </label>
+                      <input type="email"
+                        class="form-control" name="correo" required v-model="empleado.correo" id="correo" aria-describedby="helpId" placeholder="">
+                      <small id="helpId" class="form-text text-muted">Ingrese Email</small>
+                    </div>
+
+                    <div class="btn-group" role="group" aria-label="">
+                        <button type="submit" class="btn btn-success">Modificar </button>
+                        <router-link :to="{name:'Listar'}" class="btn btn-warning">Cancelar</router-link>
+                       
+                    </div>
+
+                </form>
+            </div>
+            <div class="card-footer text-muted">
+                Estudio Software
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            empleado:{}
+        }
+    },
+    created:function(){
+        this.obtenerInformacionID()
+    },
+    methods:{
+        obtenerInformacionID(){
+            fetch('http://www.svr1.ar/empleados/?consultar='+this.$route.params.id)
+            .then(respuesta=>respuesta.json())
+            .then((datosRespuesta)=>{
+                console.log(datosRespuesta)
+                this.empleado = datosRespuesta[0]
+                console.log(this.empleado)
+              
+            })
+            .catch(console.log)
+        },
+        actualizarRegistro(){
+            var datosEnviar={id:this.$route.params.id,nombre:this.empleado.nombre,correo:this.empleado.correo}
+
+             fetch('http://www.svr1.ar/empleados/?actualizar='+this.$route.params.id,{
+                 method:"POST",
+                 body:JSON.stringify(datosEnviar)
+             })
+            .then(respuesta=>respuesta.json())
+            .then((datosRespuesta=>{
+                console.log(datosRespuesta);
+                window.location.href='../listar'
+            }))
+        },
+    }
+    
+}
+</script>
